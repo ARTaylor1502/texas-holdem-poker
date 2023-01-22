88f31fba395428e3e7d3e24e87410cf8367f4f6e
Alex Taylor <alextaylor@M3777WLXDH.lan>
2023-01-22 08:46:54 +0000

    Add player seat component

---
+++ src/components/PlayerSeat.jsx
@@ -0,0 +1,9 @@
+function PlayerSeat() {
+  return (
+    <div className="seat center-align">
+      <span>Take a seat</span>
+    </div>
+  );
+}
+
+export default PlayerSeat;
--- src/components/PokerGame.jsx
+++ src/components/PokerGame.jsx
@@ -1,7 +1,15 @@
+import PlayerSeat from "./PlayerSeat";
+
 function PokerGame() {
   return (
-    <div id="poker-room">
+    <div id="poker-room" className="center-align">
+      <div id="player2-area" className="player-area center-align">
+        <PlayerSeat />
+      </div>
       <div id="poker-table"></div>
+      <div id="player1-area" className="player-area center-align">
+        <PlayerSeat />
+      </div>
     </div>
   );
 }
