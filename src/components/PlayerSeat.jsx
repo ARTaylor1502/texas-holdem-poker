8f7e4f790b4271737df4f2c45b89faf01c6cc527
Alex Taylor <alextaylor@M3777WLXDH.lan>
2023-01-22 09:11:43 +0000

    Add player avatar support and assignment on click of player seat

--- src/components/PlayerSeat.jsx
+++ src/components/PlayerSeat.jsx
@@ -1,7 +1,23 @@
-function PlayerSeat() {
+import { useState } from "react";
+
+function PlayerSeat(props) {
+  const [name, setName] = useState("");
+
+  function addPlayer() {
+    setName(props.playerName);
+  }
+
+  let seatContent;
+
+  if (name !== "") {
+    seatContent = <img src={props.avatarUrl} alt="Player avatar" />;
+  } else {
+    seatContent = <span>Take a seat</span>;
+  }
+
   return (
-    <div className="seat center-align">
-      <span>Take a seat</span>
+    <div className="seat center-align" onClick={addPlayer}>
+      {seatContent}
     </div>
   );
 }
--- src/components/PokerGame.jsx
+++ src/components/PokerGame.jsx
@@ -4,11 +4,17 @@ function PokerGame() {
   return (
     <div id="poker-room" className="center-align">
       <div id="player2-area" className="player-area center-align">
-        <PlayerSeat />
+        <PlayerSeat
+          playerName="Player Two"
+          avatarUrl="assets/images/avatars/man.svg"
+        />
       </div>
       <div id="poker-table"></div>
       <div id="player1-area" className="player-area center-align">
-        <PlayerSeat />
+        <PlayerSeat
+          playerName="Player One"
+          avatarUrl="assets/images/avatars/lady.svg"
+        />
       </div>
     </div>
   );
