7e41cf102188068d7a4831a577376fdc9f52e1e8
Alex Taylor <alextaylor@M3777WLXDH.lan>
2023-01-22 09:48:27 +0000

    Add conditional rendering of player info bar

--- src/components/PlayerSeat.jsx
+++ src/components/PlayerSeat.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function PlayerSeat(props) {
   const [name, setName] = useState("");
+  const [chipsCount, setChipsCount] = useState(500);
 
   function addPlayer() {
     setName(props.playerName);
@@ -16,8 +17,16 @@ function PlayerSeat(props) {
   }
 
   return (
-    <div className="seat center-align" onClick={addPlayer}>
-      {seatContent}
+    <div className="player-container">
+      <div className="seat center-align" onClick={addPlayer}>
+        {seatContent}
+      </div>
+      {name !== "" && (
+        <div className="player-info">
+          <div className="player-name">{name}</div>
+          <div className="player-chips">{chipsCount}</div>
+        </div>
+      )}
     </div>
   );
 }
