4509b044062ba5e3a5a91d4473b3134af7e654b9
Alex Taylor <alextaylor@M3777WLXDH.lan>
2023-01-27 21:00:31 +0000

    Add new game trigger functionality

--- src/components/PlayerSeat.jsx
+++ src/components/PlayerSeat.jsx
@@ -1,30 +1,67 @@
 import { useState } from "react";
 
 function PlayerSeat(props) {
-  const [name, setName] = useState("");
-  const [chipsCount, setChipsCount] = useState(500);
+  const [revealCards, setRevealCards] = useState(false);
 
   function addPlayer() {
-    setName(props.playerName);
+    props.setGamePlayers({ name: props.playerName, chips: 500, hand: [] });
   }
 
   let seatContent;
 
-  if (name !== "") {
+  if (props.player.length) {
     seatContent = <img src={props.avatarUrl} alt="Player avatar" />;
   } else {
     seatContent = <span>Take a seat</span>;
   }
 
+  const handleMouseOver = () => {
+    setRevealCards(true);
+  };
+
+  const handleMouseOut = () => {
+    setRevealCards(false);
+  };
+
   return (
     <div className="player-container">
+      <div className="card-area">
+        <div
+          className="player-cards"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          {props.player.length &&
+            (revealCards
+              ? props.player[0].hand.map((card, i) => {
+                  return (
+                    <img
+                      key={`${props.player[0]}.name-card${i}`}
+                      className="playing-card"
+                      src={card.img_uri}
+                      alt="Playing Card"
+                    />
+                  );
+                })
+              : props.player[0].hand.map((card, i) => {
+                  return (
+                    <img
+                      key={`${props.player[0]}.name-card${i}`}
+                      className="playing-card"
+                      src={"assets/images/cards/card-back.svg"}
+                      alt="Playing Card"
+                    />
+                  );
+                }))}
+        </div>
+      </div>
       <div className="seat center-align" onClick={addPlayer}>
         {seatContent}
       </div>
-      {name !== "" && (
+      {props.player.length && (
         <div className="player-info">
-          <div className="player-name">{name}</div>
-          <div className="player-chips">{chipsCount}</div>
+          <div className="player-name">{props.player[0].name}</div>
+          <div className="player-chips">{props.player[0].chips}</div>
         </div>
       )}
     </div>
--- src/components/PokerGame.jsx
+++ src/components/PokerGame.jsx
@@ -1,21 +1,96 @@
+import { useState } from "react";
 import PlayerSeat from "./PlayerSeat";
 
 function PokerGame() {
+  const pokerGameStages = [
+    {
+      stage_number: 1,
+      stage_name: "Pre Flop",
+      cards_in_play: 3,
+    },
+    {
+      stage_number: 2,
+      stage_name: "Flop",
+      cards_in_play: 3,
+    },
+    {
+      stage_number: 3,
+      stage_name: "Turn",
+      cards_in_play: 4,
+    },
+    {
+      stage_number: 4,
+      stage_name: "River",
+      cards_in_play: 5,
+    },
+  ];
+  const suits = ["club", "heart", "spade", "diamond"];
+
+  const [gameStage, setGameStage] = useState(false);
+  const [players, setPlayers] = useState([]);
+
+  function dealCard() {
+    const cardClub = suits[Math.floor(Math.random() * suits.length)];
+    const cardNumber = Math.floor(Math.random() * 13) + 1;
+    const imgUri = `assets/images/cards/${cardClub}${cardNumber}.svg`;
+    const cardValue = cardNumber > 10 ? 10 : cardNumber;
+    return {
+      club: cardClub,
+      number: cardNumber,
+      img_uri: imgUri,
+      value: cardValue,
+    };
+  }
+
+  function getPlayer(name) {
+    return players.filter((player) => {
+      return player.name === name;
+    });
+  }
+
+  function setGamePlayers(player) {
+    setPlayers((current) => [...current, player]);
+  }
+
+  function startNewGame() {
+    setGameStage(1);
+
+    for (let i = 0; i < players.length; i++) {
+      let player = players[i];
+
+      for (let i = 0; i <= 1; i++) {
+        player.hand.push(dealCard());
+      }
+
+      setPlayers([...players], player);
+    }
+  }
+
   return (
     <div id="poker-room" className="center-align">
       <div id="player2-area" className="player-area center-align">
         <PlayerSeat
+          player={getPlayer("Player Two")}
           playerName="Player Two"
           avatarUrl="assets/images/avatars/man.svg"
+          setGamePlayers={setGamePlayers}
         />
       </div>
       <div id="poker-table"></div>
       <div id="player1-area" className="player-area center-align">
         <PlayerSeat
+          player={getPlayer("Player One")}
           playerName="Player One"
           avatarUrl="assets/images/avatars/lady.svg"
+          setGamePlayers={setGamePlayers}
         />
       </div>
+
+      {players.length > 1 && (
+        <button id="new-game-btn" onClick={startNewGame}>
+          New Game
+        </button>
+      )}
     </div>
   );
 }
